a1575c7f9ae3811f9e2dc58b1c53af8094c70888
JoaoWudarski <joaowudarski@gmail.com>
2023-04-12 17:18:14 -0300

    Ajuste mandar para qualquer email

--- src/main/java/br/com/spotifyjvcw/host/RotineEndpoint.java
+++ src/main/java/br/com/spotifyjvcw/host/RotineEndpoint.java
@@ -6,10 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 @RestController
@@ -23,8 +20,8 @@ public class RotineEndpoint {
             @ApiResponse(responseCode = "200", description = "successful operation")
     })
     @GetMapping("/mail/{clientId}")
-    public ResponseEntity<Void> sendMailWhenChanged(@PathVariable String clientId){
-        sendMail.whenChangePositions(clientId);
+    public ResponseEntity<Void> sendMailWhenChanged(@PathVariable String clientId, @RequestParam(required = false) String sendEmail){
+        sendMail.whenChangePositions(clientId, sendEmail);
         return ResponseEntity.ok().build();
     }
 }
